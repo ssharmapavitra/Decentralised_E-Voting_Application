fb10d7afa50a90aa335b4e6939c2549e911d2a0a
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2022-06-05 15:03:37 +0530

    .
    The basic structure is created. Bootstrap and other components added.

---
+++ DEVA/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+function App() {
+	return (
+		<>
+			<nav className="navbar navbar-light bg-light">
+				<a href="#" className="navbar-brand mr-auto">
+					BlockChain Polls
+				</a>
+				<button type="button" className="btn btn-secondary">
+					Create Poll
+				</button>
+			</nav>
+			<section className="jumpotron bg-light text-center">
+				<h1>Polls Rethinked</h1>
+				<p className="lead text-muted">Powered by Blockchain</p>
+			</section>
+			<div className="d-flex">
+				<div className="container poll-list">List</div>
+				<div className="container poll-detail">Detail</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
