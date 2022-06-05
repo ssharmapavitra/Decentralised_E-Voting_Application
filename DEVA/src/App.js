f3ee43a7e01343755a8e7a7e537ad9d047c6c78e
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2022-06-05 15:21:16 +0530

    Update App.js

--- DEVA/src/App.js
+++ DEVA/src/App.js
@@ -3,7 +3,7 @@ function App() {
 	return (
 		<>
 			<nav className="navbar navbar-light bg-light">
-				<a href="#" className="navbar-brand mr-auto">
+				<a href="/" className="navbar-brand mr-auto">
 					BlockChain Polls
 				</a>
 				<button type="button" className="btn btn-secondary">
